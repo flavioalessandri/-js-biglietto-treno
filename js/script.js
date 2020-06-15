736e11f77a3d77860e7bcf674a140e410784eb36
Flavio <flavio.alessandri@gmail.com>
2020-06-15 17:38:20 +0200

    JS - new Formula with isNan

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 //chiedere km da percorere a utente salvare dato
-var under18Cost;
-var over65Cost;
+var finalCost;
 var cost = 21;
 console.log ("il costo è di " ,  (cost/100) , "€/km");
 
-var kmTraveled = parseInt(prompt("Digita qui i km da percorrere"));
+var kmTraveled = parseInt(prompt("Digita qui i km da percorrere" , "es. 200"));
 console.log ("Km da percorrere " ,kmTraveled, "km");
 
+
 //chiedere età a utente e salvare dato
-var userAge = parseInt(prompt("Digita qui la tua età"));
+var userAge = parseInt(prompt("Digita qui la tua età" , "es. 30"));
 console.log ("Età utente ",userAge, "anni");
 
-alert ("Attendi il calcolo della tua tariffa");
+
+
+// controllo valori
+
+if (isNaN(userAge) || isNaN(kmTraveled) ) {
+    alert("Input not valid");
+  } else {
+    alert ("Attendi il calcolo della tua tariffa");
+  }
 
 //calcolare costo al km(km * 0,21euro aprossimando il risultato per difetto)
 
@@ -27,23 +35,32 @@ console.log ("lo sconto del 40% risulta di ", sale40percent , "€");
 //se età digitata è inferiore a 18 anni applicare sconto del 20%
 //restituire valore di tariffa base per far capire all'utente il risparmio
 
-//se età digitata è superiore a 65 anni applicare sconto del 40%
-//restituire valore di tariffa base per far capire all'utente il risparmio
+if ((userAge < 18) && (userAge !== 0)) {
+  finalCost = (kmCost - sale20percent).toFixed(2);
+  console.log ("il costo per gli under 18 è di " , finalCost , "€");
+  document.getElementById("ticket-rate").innerHTML= "TARIFFA 'YOUNG'";
+  document.getElementById("cost").innerHTML=  finalCost  + " €";
+  document.getElementById("subtitle").innerHTML= "Hai risparmiato " + sale20percent + " € sulla tariffa base!";
+
+  //se età digitata è superiore a 65 anni applicare sconto del 40%
+  //restituire valore di tariffa base per far capire all'utente il risparmio
+
+
+} else if ((userAge > 65) && (userAge !== 0)){
+  finalCost = (kmCost - sale40percent).toFixed(2);
+  console.log ("il costo per gli over 65 è di " , finalCost , "€");
+  document.getElementById("ticket-rate").innerHTML= "TARIFFA 'CONFORTABLE'";
+  document.getElementById("cost").innerHTML=  finalCost  + " €";
+  document.getElementById("subtitle").innerHTML= "Hai risparmiato " + sale40percent + " € sulla tariffa base!";
+
+  //resitutisci valore errato se viene digitato 0 oppure se non viene digitato niente
+
+} else if ((isNaN(userAge) || isNaN(kmTraveled)) || ((userAge === 0) || (kmTraveled === 0)) ) {
+  document.getElementById("ticket-rate").innerHTML= "Non è stato possibile calcolare il costo a causa di valori inseriti errati!";
 
 //in alternativa restituire valore soltanto costo tariffa base
 
-if (userAge < 18) {
-  finalCost = (kmCost - sale20percent).toFixed(2);
-  console.log ("il costo per gli under 18 è di " , finalCost , "€");
-  document.getElementById("title").innerHTML= "il costo per gli under18 è di " + finalCost + " €";
-  document.getElementById("subtitle").innerHTML= "Hai risparmiato " + sale20percent + " € sulla tariffa base";
-} else if (userAge > 65){
-  finalCost = (kmCost - sale40percent).toFixed(2);
-  console.log ("il costo per gli over 65 è di " , finalCost , "€");
-  document.getElementById("title").innerHTML= "il costo per gli over 65 è di " + finalCost + " €";
-    document.getElementById("subtitle").innerHTML= "Hai risparmiato " + sale40percent + " € sulla tariffa base";
-} else{
-  finalCost === kmCost;
-  console.log (finalCost);
-  document.getElementById("title").innerHTML= "Al tuo biglietto verrà applicata la tariffa base di " + finalCost + " €";
+} else  {
+  document.getElementById("ticket-rate").innerHTML= "TARIFFA BASE";
+ document.getElementById("subtitle").innerHTML= "Gentile cliente, al tuo biglietto verrà applicato il costo di " + kmCost + " €";
 }
